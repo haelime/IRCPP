db3623d8fdfaeb3279910bbd0c29f7689fe1f4a8
Hyunjun Kim <hyunjun2372@gmail.com>
2024-03-24 17:26:54 +0900

    Refactor names and comments for readability (#1)
    * Refactor names and comments for readability
    
    * Fix compile error

--- include/Server.hpp
+++ include/Server.hpp
@@ -44,13 +44,14 @@ public:
     { 
         std::cerr << "Usage : " << argv[0] << " <port> <password>\n";
     }
-    bool checkAndSetArgv(int argc, char** argv); 
-    void init_server(void);
+    bool initServer(int argc, char** argv);
     void run();
     void stop(){}; // TODO : implement
 
 
 private:
+    bool setPortAndPassFromArgv(int argc, char** argv); 
+
     void assembleDataToMessage(std::pair<SOCKET_FD, std::string>& data);
 
     bool isValidMessage(std::string& data);
@@ -61,33 +62,26 @@ private:
 
 
 
-    // server network data
-private:
+private:  // server network data
     SOCKET_FD mServerListenSocket;
     sockaddr_in mServerAddress;
     socklen_t mServerAddressLength;
 
-    KQUEUE_FD mhKq;
-    // kernal monitor events
-    struct kevent mEvent;
-    // client events
-    std::vector<struct kevent> mEventVector;
+    KQUEUE_FD mhKqueue;
 
-private: // arguments
+private: // server configuration
     int mPort;
     std::string mServerPassword;
 
      
 private: // server data
-    // ClientData has is's channel information, so we don't need to store channel information in server
 
-    std::map<SOCKET_FD, ClientData*>    mFdToEveryClientDataMap;
-    std::map<std::string, Channel*>     mNameToEveryChannelMap;
+    std::map<SOCKET_FD, ClientData*>    mFdToClientGlobalMap;
+    std::map<std::string, Channel*>     mNameToChannelGlobalMap;
 
-    std::queue<std::pair<SOCKET_FD, std::string> > mServerDataQueue;
-
+    std::queue<std::pair<SOCKET_FD, std::string> > mClientRecvMsgQueue;
 
     time_t mServerStartTime;
-    time_t mServerLastPingTime; // to kick if not received in 2 seconds
+    time_t mServerLastPingTime; //< to kick if not received in 2 seconds
 
 };
